97341f027b2d9098d6cba03ebf640a0c8743f483
meiengineers2025-coder <meiengineers2025@gmail.com>
2025-11-11 23:48:11 +0530

    Add files via upload

---
+++ access.js
@@ -0,0 +1,31 @@
+// src/utils/access.js
+
+// Checks if user is logged in
+function requireAuth(req, res, next) {
+  if (!req.session.user) {
+    return res.redirect("/login");
+  }
+  next();
+}
+
+// Checks if user is a candidate
+function isCandidate(req, res, next) {
+  if (req.session.user?.role !== "candidate") {
+    return res.status(403).send("Access denied");
+  }
+  next();
+}
+
+// Checks if user is an employer
+function isEmployer(req, res, next) {
+  if (req.session.user?.role !== "employer") {
+    return res.status(403).send("Access denied");
+  }
+  next();
+}
+
+module.exports = {
+  requireAuth,
+  isCandidate,
+  isEmployer,
+};
